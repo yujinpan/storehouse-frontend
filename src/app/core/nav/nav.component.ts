cba886c29d64b00ad02eb91de75495ed393eb11b
yujinpan <1192878390@qq.com>
2018-11-28 11:27:35 +0800

    chore: 修改导航的布局

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+
+import { MenuService } from "../menu.service";
 
 @Component({
   selector: 'app-nav',
@@ -8,19 +10,18 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class NavComponent implements OnInit {
   @Output() menuStateChange = new EventEmitter<boolean>();
 
+  private menus;
   private logoActive: boolean = true;
 
-  private menus = [
-    { name: '工具', url: '.' }
-  ];
+  constructor(menuService: MenuService) {
+    this.menus = menuService.getMenus();
+  }
 
   ngOnInit() {
-
   }
 
   private toggleMenu() {
     this.logoActive = !this.logoActive;
     this.menuStateChange.emit(this.logoActive);
   }
-
 }
