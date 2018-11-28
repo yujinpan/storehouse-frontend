5071373e6597023844dc30050cb8165627f3b3e6
yujinpan <1192878390@qq.com>
2018-11-28 11:27:01 +0800

    feat: 增加响应式菜单布局与交互

---
+++ src/app/core/slide-nav/slide-nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MenuService } from '../menu.service';
+
+@Component({
+  selector: 'app-slide-nav',
+  templateUrl: './slide-nav.component.html',
+  styleUrls: ['./slide-nav.component.less']
+})
+export class SlideNavComponent implements OnInit {
+  private menus;
+
+  constructor(menuService: MenuService) {
+    this.menus = menuService.getMenus();
+  }
+
+  ngOnInit() {
+  }
+
+}
